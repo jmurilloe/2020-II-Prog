01e990c65a3b8499130330e52ccc0439ca284839
jmurilloe <jmurilloe@unal.edu.co>
2020-08-30 12:16:41 -0500

    se añaden formatos de comentario

--- 2020-08-28-git/ejemplo.cpp
+++ 2020-08-28-git/ejemplo.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 int main(void)
+// comentario, unicamente selecciona la linea como tal por //
+/*
+esto deberia ser tambien un comentario que inicia con /* y termina con el mismo simbolo pero al reves. Note que este formato no tiene limite de lineas 
+ */
+  
 {
   std::cout << "Hello World!\n";
   std::cout << "Hola Mundo!\n";
