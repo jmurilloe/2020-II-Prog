efdbb6a587f64e8dda09a432918833a79d68d9b5
jmurilloe <jmurilloe@unal.edu.co>
2020-09-09 00:56:44 -0500

    Se añade programa de aproximacion de Euler para EDO

---
+++ 2020-09-08-auto/MetodoEulerEDO.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<cmath>
+
+int main ()
+{
+    int n, x=0;
+    double h;
+    long double x0, y0;
+    std::cout << "Please enter the number of steps (n):" << std::endl; 
+    std::cin >> n;
+    std::cout << "Please enter your step proximity (h):" << std::endl;
+    std::cin >> h;
+    std::cout << "Please enter your independent variable for IVP (x0):" << std::endl;
+    std::cin >> x0;
+    std::cout << "Please enter you dependent variable for IVP (y0):" << std::endl;
+    std::cin >> y0;
+    std::cout << "The points (xn, yn) Euler's method has developed, are:" << std::endl;
+    
+    while (x<=n)
+    {
+        std::cout << "(x" << x << ", y" << x << ") = "  << "(" << x0 << ", " << y0 << ")" << std::endl;
+        y0 += (h*(3+x0-y0));
+        x++;
+        x0 += h;
+
+    }
+    return 0;
+}    
