5c0648d2b0e91c1877b8424fbad0e788abbb91b6
jmurilloe <jmurilloe@unal.edu.co>
2020-09-10 17:59:14 -0500

    Se añaden variables locales y globales

---
+++ 2020-09-10-funciones/ejemplo1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+const int a = 20;
+
+void print_message (int a)
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Parametro a:\n" << a << "\n"; 
+    std::cout << "Direccion de a en print: " << &a << std::endl;
+}
+
+int main (void)
+{
+    int a = 12;
+    std::cout << "Direccion de a en main: " << &a << std::endl;
+    print_message(a);
+    return 0;
+}
+//se menciona el uso de variables locales y globales. EL profesor dice que
+//prefiere las locales. Aun asi, para declarar una variable "constante" global (que no
+//modifica a lo largo del codigo de la forma const (tipo) (nombre)
